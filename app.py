bc6e9224d3e426c2b80462caacf7111db412f699
Ajay Patil <patilaja@gmail.com>
2020-08-09 11:32:53 -0400

    Cloud Deployment Readiness files

--- app.py
+++ app.py
@@ -1,23 +1,61 @@
+# Import libraries
 from flask import Flask, jsonify, render_template
+from dataBase.dbFunction import *
+
+modelFile = 'sentiment_model.pickle'
+
 app = Flask(__name__)
 
+#Take care of page not found
+@app.errorhandler(404)
+
+# Use inbuilt function which takes error as parameter
+def not_found(e):
+  #redirect to home page - we can always show custom page
+    return render_template("index.html", title="Tweeter Sentiment Analysis")
+
 @app.route("/")
-def welcome():
-    return render_template("index.html")
+@app.route("/sentimentAnalysis")
+def sentimentAnalysis():
+    return jsonify(tweetsPredictData())
+
+@app.route("/tweetData")
+def tweetData():
+    return jsonify(tweetsData())
+
+@app.route("/tweetUser")
+def tweetUser():
+    return jsonify(tweetUser())
+
+@app.route("/twitterSource")
+def twitterSource():
+    return jsonify(twitterSource())
 
 
-@app.route("/tools")
-def sources():
-    return render_template("tools.html")
+@app.route("/predictSentiment",methods=['POST','GET')
+def predictSentiment():
+     if request.method == 'POST':
+        try:
+            # Reading the inputs given by the user
+            text=float(request.form['tweet'])
+            
+            # Loading the model file  
+            loaded_model = pickle.load(open(modelFile, 'rb')) 
 
+            # Predict sentiment
+            prediction = loaded_model.predict(text)
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+            # Print prediction
+            print('prediction is', prediction)
 
-@app.route("/mlmodels")
-def mlmodels():
-    return render_template("mlmodels.html")
+            # Show the prediction results in a UI
+            return render_template('prediction.html',prediction=prediction))
+        except Exception as e:
+            print('The Exception message is: ',e)
+            return 'Error occured during predicting results'
+    else:
+        return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
