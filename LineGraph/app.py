39e26c85d5b9a853816ec744511f8e1c480214fd
Archna Ashish <ashisharchna@gmail.com>
2020-08-10 20:18:28 -0400

    updating the app.py

---
+++ LineGraph/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify, render_template
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return render_template("index.html")
+
+@app.route("/doughnut-data")
+def doughnut_data():
+    df=pd.read_csv("../Resources/data/tweetPredData.csv")
+    df=df.drop(["Favorite", "predTweet", "CleanedTweet", "Source"],axis=1)
+    output=df.to_json(orient="records")
+    return  output
+
+@app.route("/tools")
+def sources():
+    return render_template("tools.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/mlmodels")
+def mlmodels():
+    return render_template("mlmodels.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
